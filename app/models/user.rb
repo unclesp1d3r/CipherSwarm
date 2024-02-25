ba436d349310c6b43f285a730d7f781bc62b6a75
UncleSp1d3r <unclespider@protonmail.com>
2024-02-25 17:24:27 -0500

    Setup Rubocop, brakeman, and workflows
    Signed-off-by: UncleSp1d3r <unclespider@protonmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :confirmable, :lockable, :timeoutable, :trackable
+         :lockable, :timeoutable, :trackable
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
@@ -73,7 +75,6 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
-  
 
   # Configure the default URL for Action Mailer (required for Devise)
   # Make use of the MailCatcher gem to catch and display emails in development
