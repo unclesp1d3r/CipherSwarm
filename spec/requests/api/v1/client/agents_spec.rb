3eba0f6b0264faeae77c265c7fdc50a00e0cbf9a
UncleSp1d3r <unclespider@protonmail.com>
2024-03-02 00:03:18 -0500

    Added additional basic tests
    Signed-off-by: UncleSp1d3r <unclespider@protonmail.com>

--- spec/requests/api/v1/client/agents_spec.rb
+++ spec/requests/api/v1/client/agents_spec.rb
@@ -4,9 +4,16 @@ require 'rails_helper'
 
 RSpec.describe "Api::V1::Client::Agents", type: :request do
   describe "GET /show" do
-    it "returns http forbidden" do
+    it "returns http forbidden when anonymous" do
       get "/api/v1/client/agents/show"
       expect(response).to have_http_status(:forbidden)
+    end
+
+    it "returns http success when authenticated" do
+      user = create(:user)
+      agent = create(:agent)
+      get "/api/v1/client/agents/show", params: { token: agent.token }
+      expect(response).to have_http_status(:success)
     end
   end
 
