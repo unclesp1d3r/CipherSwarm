f2764f313d1abd05f25c8694bdfae0fe0082522f
UncleSp1d3r <unclespider@protonmail.com>
2024-03-02 00:03:18 -0500

    Added additional basic tests
    Signed-off-by: UncleSp1d3r <unclespider@protonmail.com>

--- spec/requests/admin_spec.rb
+++ spec/requests/admin_spec.rb
@@ -2,9 +2,23 @@ require 'rails_helper'
 
 RSpec.describe "Admins", type: :request do
   describe "GET /index" do
-    it "returns http success" do
+    it "returns http redirect" do
       get "/admin/index"
       expect(response).to have_http_status(401)
+    end
+
+    it "returns http success on authenticated admin" do
+      user = create(:user, role: :admin)
+      sign_in user
+      get "/admin/index"
+      expect(response).to have_http_status(:success)
+    end
+
+    it "returns http failure on authenticated non-admin" do
+      user = create(:user, role: :basic)
+      sign_in user
+      get "/admin/index"
+      expect(response).to have_http_status(:unauthorized)
     end
   end
 end
