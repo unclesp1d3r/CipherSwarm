4765725093cfcf8ec8979ae033aef2e8b4042e03
UncleSp1d3r <unclespider@protonmail.com>
2024-03-01 20:15:19 -0500

    Minor cleanup and added devices column to agents
    Signed-off-by: UncleSp1d3r <unclespider@protonmail.com>

--- app/models/agent.rb
+++ app/models/agent.rb
@@ -8,11 +8,12 @@
 #  client_signature(The signature of the agent)                                                 :text
 #  command_parameters(Parameters to be passed to the agent when it checks in)                   :text
 #  cpu_only(Only use for CPU only tasks)                                                        :boolean          default(FALSE)
+#  devices(Devices that the agent supports)                                                     :string           default([]), is an Array
 #  ignore_errors(Ignore errors, continue to next task)                                          :boolean          default(FALSE)
 #  last_ipaddress(Last known IP address)                                                        :string           default("")
 #  last_seen_at(Last time the agent checked in)                                                 :datetime
 #  name(Name of the agent)                                                                      :string           default("")
-#  operating_system(Operating system of the agent)                                              :integer          default("linux")
+#  operating_system(Operating system of the agent)                                              :integer          default(0)
 #  token(Token used to authenticate the agent)                                                  :string(24)       indexed
 #  trusted(Is the agent trusted to handle sensitive data)                                       :boolean          default(FALSE)
 #  created_at                                                                                   :datetime         not null
@@ -34,5 +35,5 @@ class Agent < ApplicationRecord
   validates_presence_of :name
   validates_presence_of :user
 
-  enum operating_system: [ unknown: 0, linux: 1, windows: 2, macos: 3, other: 4 ]
+  enum operating_system: { unknown: 0, linux: 1, windows: 2, macos: 3, other: 4 }
 end
