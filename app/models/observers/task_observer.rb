f9103cdf9b0478765501b82ad0a6d756e9bacf9a
UncleSp1d3r <unclespider@protonmail.com>
2024-04-04 20:41:57 -0400

    Moved to a proper state machine for tasks & attacks
    Signed-off-by: UncleSp1d3r <unclespider@protonmail.com>

--- app/models/observers/task_observer.rb
+++ app/models/observers/task_observer.rb
@@ -1,6 +1,14 @@
 # frozen_string_literal: true
 
-class TaskObserver
+class TaskObserver < ActiveRecord::Observer
+  def after_update(task)
+    task.update_activity_timestamp
+  end
+
+  def after_accept(task)
+    task.update_activity_timestamp
+  end
+
   def after_accept_crack(task)
     task.update_activity_timestamp
   end
