e0ada5b1001407dfefa34340a1028d1bd914aa0b
UncleSp1d3r <unclespider@protonmail.com>
2024-04-16 20:52:15 -0400

    Resolve Issue #55
    Signed-off-by: UncleSp1d3r <unclespider@protonmail.com>

---
+++ config/initializers/sidekiq_monkey_patch.rb
@@ -0,0 +1,7 @@
+Rails.application.config.after_initialize do
+  Turbo::Streams::BroadcastStreamJob.class_eval do
+    def self.perform_later(stream, content:)
+      super(stream, content: content.to_str)
+    end
+  end
+end
