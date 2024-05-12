0f249cb3c095db8dca0b9ed885910017de35e175
UncleSp1d3r <unclespider@protonmail.com>
2024-05-11 21:04:52 -0400

    Moved from importmap to esbuild
    Signed-off-by: UncleSp1d3r <unclespider@protonmail.com>

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,7 +1,5 @@
 //= link_tree ../images
-//= link_tree ../../javascript .js
-//= link_tree ../../../vendor/javascript .js
-//= link_tree ../builds
+// = link_tree ../builds
 
 //= link administrate-field-active_storage/application.css
 //= link administrate-field-jsonb/application.css
