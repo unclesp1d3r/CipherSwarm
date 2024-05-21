ec9778ee5dcd5c24ba8abe24cefbf798d7dc2258
UncleSp1d3r <unclespider@protonmail.com>
2024-05-20 20:52:31 -0400

    refactor: dry-ed out the view components
    Signed-off-by: UncleSp1d3r <unclespider@protonmail.com>

--- app/components/navbar_dropdown_component.rb
+++ app/components/navbar_dropdown_component.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-class NavbarDropdownComponent < ViewComponent::Base
-  def initialize(title:)
-    @title = title
-  end
+class NavbarDropdownComponent < ApplicationViewComponent
+  option :title, required: true
+  option :extra_classes, optional: true, default: nil
+  option :icon, optional: true, default: nil
 end
