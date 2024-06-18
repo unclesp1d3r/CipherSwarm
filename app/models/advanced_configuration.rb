62214e2bb464a1ccab982552f7da635078ee1896
UncleSp1d3r <unclespider@protonmail.com>
2024-06-17 21:03:07 -0400

    feat: Added support for sending OpenCL device limits
    Signed-off-by: UncleSp1d3r <unclespider@protonmail.com>

--- app/models/advanced_configuration.rb
+++ app/models/advanced_configuration.rb
@@ -5,5 +5,6 @@ class AdvancedConfiguration
   attribute :agent_update_interval, :integer, default: -> { rand(5..15) }
   attribute :use_native_hashcat, :boolean, default: false
   attribute :backend_device, :string
+  attribute :opencl_devices, :string
   attribute :enable_additional_hash_types, :boolean, default: false
 end
