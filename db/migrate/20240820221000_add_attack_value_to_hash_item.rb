5bfa7758926b59e7ea7a2ded75f2c9a21ccff235
UncleSp1d3r <unclespider@protonmail.com>
2024-08-20 18:35:45 -0400

    fix: Removed constraint on duplicate hashes
    There was a weird bug where the hash item would get created as a duplicate but could never be updated or cracked because it didn’t meet validation. While fixing this, I realized that we might actually want duplicate hashes because there may be multiple users with the same password in a dump, and the metadata would differentiate them.
    
    Signed-off-by: UncleSp1d3r <unclespider@protonmail.com>

--- db/migrate/20240820221000_add_attack_value_to_hash_item.rb
+++ db/migrate/20240820221000_add_attack_value_to_hash_item.rb
@@ -1,4 +1,7 @@
+# frozen_string_literal: true
+
 class AddAttackValueToHashItem < ActiveRecord::Migration[7.1]
   def change
+    add_reference :hash_items, :attack, null: true, foreign_key: true, comment: "The attack that cracked this hash"
   end
 end
