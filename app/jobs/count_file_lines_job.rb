14d5c533a767f3f0c727540e26e6035fbf6dc36f
UncleSp1d3r <unclespider@protonmail.com>
2024-09-04 18:15:38 -0400

    fix: Updated the count file lines job to simplify it.
    The job was unnecessarily complex and did not always function reliably if something glitched. I simplified it to improve reliability and updated it to work with RSpec 7.
    
    Signed-off-by: UncleSp1d3r <unclespider@protonmail.com>

--- app/jobs/count_file_lines_job.rb
+++ app/jobs/count_file_lines_job.rb
@@ -1,31 +1,28 @@
 # frozen_string_literal: true
 
+# CountFileLinesJob is a background job that counts the number of lines in a file
+# associated with a given record and updates the record with the line count.
+#
+# This job retries on ActiveStorage::FileNotFoundError and ActiveRecord::RecordNotFound
+# with polynomially increasing wait times.
 class CountFileLinesJob < ApplicationJob
   queue_as :high
   retry_on ActiveStorage::FileNotFoundError, wait: :polynomially_longer, attempts: 10
   retry_on ActiveRecord::RecordNotFound, wait: :polynomially_longer, attempts: 2
 
-  # Performs the job to count the number of lines in a file associated with a given list.
+  # Performs the job.
   #
-  # @param args [Array] The arguments passed to the job.
-  # @return [void]
-  def perform(*args)
-    id = args.first
-    type = args.second
+  # @param id [Integer] the ID of the record to process
+  # @param type [String] the class name of the record to process
+  def perform(id, type)
     klass = type.constantize
     list = klass.find(id)
     return if list.nil?
     return if list.processed? || list.file.nil?
 
     list.file.open do |file|
-      count = 0
-      file.each_line do |line|
-        count += 1
-        line.chomp!
-      end
-      list.line_count = count
+      count = file.each_line.count
+      list.update!(line_count: count, processed: true)
     end
-    list.processed = true
-    list.save!
   end
 end
