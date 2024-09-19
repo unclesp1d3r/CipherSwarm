c940d9cd8a221e38049fd999395fc54c0d015c95
UncleSp1d3r <unclespider@protonmail.com>
2024-09-18 23:27:56 -0400

    refactor: correct typo in priority enum comment
    Fixed a typo in the campaign priority enum comment within the migration. Changed 'Defered' to 'Deferred' to ensure accurate documentation.

--- db/migrate/20240917024147_add_priority_enum_to_campaign.rb
+++ db/migrate/20240917024147_add_priority_enum_to_campaign.rb
@@ -3,6 +3,6 @@
 class AddPriorityEnumToCampaign < ActiveRecord::Migration[7.2]
   def change
     add_column :campaigns, :priority, :integer, default: 0, null: false,
-                                                comment: ' -1: Defered, 0: Routine, 1: Priority, 2: Urgent, 3: Immediate, 4: Flash, 5: Flash Override'
+                                                comment: ' -1: Deferred, 0: Routine, 1: Priority, 2: Urgent, 3: Immediate, 4: Flash, 5: Flash Override'
   end
 end
