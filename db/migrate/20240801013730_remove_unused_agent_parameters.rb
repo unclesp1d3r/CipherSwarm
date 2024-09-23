659f02f74978b8676dd237c7bd9f0f44e031b2bb
UncleSp1d3r <unclespider@protonmail.com>
2024-09-22 23:11:59 -0400

    refactor: optimize database migrations for hash_items and agents
    Apply bulk changes in database migrations for hash_items and agents tables. This improvement uses the change_table method, streamlining the removal and addition of columns.

--- db/migrate/20240801013730_remove_unused_agent_parameters.rb
+++ db/migrate/20240801013730_remove_unused_agent_parameters.rb
@@ -2,9 +2,11 @@
 
 class RemoveUnusedAgentParameters < ActiveRecord::Migration[7.1]
   def change
-    remove_column :agents, :command_parameters, :text
-    remove_column :agents, :cpu_only, :boolean, default: false, null: false
-    remove_column :agents, :ignore_errors, :boolean, default: false, null: false
-    remove_column :agents, :trusted, :boolean, default: false, null: false
+    change_table :agents, bulk: true do |t|
+      t.remove :command_parameters, :text
+      t.remove :cpu_only, :boolean, default: false, null: false
+      t.remove :ignore_errors, :boolean, default: false, null: false
+      t.remove :trusted, :boolean, default: false, null: false
+    end
   end
 end
