d2821741c826cf499cae93911bc2f31cb3ea67c1
UncleSp1d3r <unclespider@protonmail.com>
2024-09-23 20:45:21 -0400

    feat: add toggle for hiding completed activities
    Implemented a toggle button for users to hide or show completed activities in campaigns and attacks. Updated the user model and routes to support this feature and enhanced the UI with the necessary components.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,4 +3,10 @@
 class HomeController < ApplicationController
   # Renders the home page.
   def index; end
+
+  def toggle_hide_completed_activities
+    authorize! :read, current_user
+    current_user.toggle_hide_completed_activities
+    render partial: "campaigns/hide_completed_activities_toggle"
+  end
 end
--- config/routes/devise.rb
+++ config/routes/devise.rb
@@ -4,4 +4,5 @@ devise_for :users, skip: [:registrations]
 as :user do
   get "users/edit" => "devise/registrations#edit", :as => "edit_user_registration"
   put "users" => "devise/registrations#update", :as => "user_registration"
+  post "toggle_hide_completed_activities" => "home#toggle_hide_completed_activities"
 end
