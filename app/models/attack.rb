f47eb2fa4c89f8ce47a1fe585c0b8457a21cd25d
UncleSp1d3r <unclespider@protonmail.com>
2024-09-25 21:33:15 -0400

    docs: simplify and restructure Attack model documentation
    Refactored the documentation for the Attack model by removing unnecessary details and organizing it for better readability. Improved the structure by grouping related sections and adding concise explanations to methods and state transitions.

--- app/models/attack.rb
+++ app/models/attack.rb
@@ -1,51 +1,9 @@
 # frozen_string_literal: true
 
-# The Attack class represents an attack model in the CipherSwarm application.
-# It includes various associations, validations, enumerations, scopes, and state machine configurations.
-# The class also provides methods to estimate complexity, finish time, and generate parameters for Hashcat.
+##
+# The Attack class represents an attack in the system.
+# It maintains the relationships to other models and handles the state and behavior of an attack.
 #
-# Associations:
-# - belongs_to :campaign
-# - has_many :tasks
-# - has_many :hash_items
-# - has_one :hash_list
-# - belongs_to :rule_list
-# - belongs_to :mask_list
-# - belongs_to :word_list
-#
-# Validations:
-# - Validates presence, inclusion, length, and numericality of various attributes.
-# - Conditional validations based on attack mode (dictionary, mask, hybrid_dictionary, hybrid_mask).
-#
-# Enumerations:
-# - attack_mode: { dictionary: 0, mask: 3, hybrid_dictionary: 6, hybrid_mask: 7 }
-#
-# Scopes:
-# - default_scope: Orders by created_at
-# - pending: Filters by pending state
-# - incomplete: Filters by incomplete states
-#
-# State Machine:
-# - Initial state: pending
-# - Events: accept, run, complete, pause, error, exhaust, cancel, reset, resume, abandon
-# - States: paused, failed, exhausted, pending
-# - Callbacks: after_transition, before_transition
-#
-# Methods:
-# - estimated_complexity: Calculates the estimated complexity of an attack based on the attack mode.
-# - estimated_finish_time: Estimates the finish time of the attack.
-# - executing_agent: Returns the name of the agent associated with the most recently updated running task.
-# - hash_type: Returns the hash mode of the associated campaign's hash list.
-# - hashcat_parameters: Generates a string of parameters to be used with Hashcat based on the attributes of the Attack model.
-# - percentage_complete: Calculates the percentage of completion for the running task.
-# - run_time: Calculates the duration between the start and end times.
-# - to_label: Returns a string representation of the attack instance, combining the name and attack mode.
-#
-# Private Methods:
-# - complete_hash_list: Completes the hash list for the campaign if there are no uncracked hashes left.
-# - pause_tasks: Pauses all tasks that are not currently in the paused state.
-# - resume_tasks: Resumes all tasks associated with the current object.
-# - validate_mask_or_mask_list: Validates the presence of either `mask` or `mask_list`.
 # == Schema Information
 #
 # Table name: attacks
@@ -103,9 +61,11 @@
 #  fk_rails_...  (word_list_id => word_lists.id) ON DELETE => cascade
 #
 class Attack < ApplicationRecord
-  acts_as_paranoid
+  acts_as_paranoid # Soft deletes the attack
 
+  ##
   # Associations
+  #
   belongs_to :campaign, counter_cache: true
   has_many :tasks, dependent: :destroy, autosave: true
   has_many :hash_items, dependent: :nullify
@@ -130,6 +90,9 @@ class Attack < ApplicationRecord
   validates :workload_profile, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 4 }
   validates :mask, length: { maximum: 512 }, allow_nil: true
 
+  ##
+  # Conditional Associations
+  #
   with_options if: -> { dictionary? } do
     validates :word_list, presence: true
     validates_associated :word_list
@@ -169,85 +132,125 @@ class Attack < ApplicationRecord
   # Enumerations
   enum :attack_mode, { dictionary: 0, mask: 3, hybrid_dictionary: 6, hybrid_mask: 7 }
 
+  ##
+  # Scopes
+  #
   default_scope { order(:complexity_value, :created_at) } # We want the highest priority attack first
   scope :pending, -> { with_state(:pending) }
   scope :incomplete, -> { without_states(:completed, :exhausted, :running, :paused) }
+  scope :active, -> { with_states(:running, :pending) }
+
+  # Broadcasts a refresh to the client when the attack is updated unless running in test environment
 
   broadcasts_refreshes unless Rails.env.test?
 
-  delegate :uncracked_count, to: :campaign, allow_nil: true
+  ##
+  # Delegations
+  #
+  delegate :uncracked_count, to: :campaign, allow_nil: true # Delegates the uncracked_count method to the campaign
 
   # Callbacks
-  after_create_commit :update_stored_complexity
+  after_create_commit :update_stored_complexity # Updates the stored complexity value after the attack is created
 
-  # State machine
+  ##
+  # State Machine
+  #
   state_machine :state, initial: :pending do
+    # Trigger that an agent has accepted the attack. This can only be triggered when the attack is in the pending state.
     event :accept do
       transition all - %i[completed exhausted] => :running
     end
 
+    # Trigger that the attack has been started. This can only be triggered when the attack is in the pending state.
     event :run do
       transition pending: :running
     end
 
+    # Trigger that the attack has been completed. If the attack is in the running state, it will transition to the completed state
+    # if all tasks are completed or the hash list is fully cracked. If the attack is in the pending state, it will transition to the
+    # completed state if the hash list is fully cracked.
     event :complete do
       transition running: :completed if ->(attack) { attack.tasks.all?(&:completed?) || attack.campaign.completed? }
       transition pending: :completed if ->(attack) { attack.hash_list.uncracked_count.zero? }
       transition all - [:running] => same
     end
 
+    # Trigger that the attack has been paused. If the attack is in the running or pending state, it will transition to the paused state.
     event :pause do
-      transition any - %i[completed exhausted] => :paused
+      transition %i[running pending] => :paused
       transition any => same
     end
 
+    # Trigger that the attack has encountered an error. If the attack is in the running state, it will transition to the failed state.
     event :error do
       transition running: :failed
       transition any => same
     end
 
+    # Trigger that the attack has been exhausted. If the attack is in the running state, it will transition to the completed state
+    # if all tasks are exhausted. If the attack is in the running state, it will transition to the completed state if the hash list
+    # is fully cracked.
     event :exhaust do
       transition running: :completed if ->(attack) { attack.tasks.all?(&:exhausted?) }
       transition running: :completed if ->(attack) { attack.hash_list.uncracked_count.zero? }
-      transition all - [:running] => same
+      transition any => same
     end
 
+    # Trigger that the attack has been canceled. If the attack is in the pending or running state, it will transition to the failed state.
     event :cancel do
       transition %i[pending running] => :failed
     end
 
+    # Trigger that the attack has been reset. If the attack is in the failed, completed, or exhausted state, it will transition to the pending state.
+    # This is only used when the attack needs to be re-run, such as when it has been modified, the hash list has changed, etc.
     event :reset do
       transition %i[failed completed exhausted] => :pending
     end
 
+    # Trigger that the attack is being resumed. If the attack is in the paused state, it will transition to the pending state.
     event :resume do
       transition paused: :pending
       transition any => same
     end
 
+    # Trigger that the agent has abandoned the attack. If the attack is in the running state, it will transition to the pending state.
+    # This is to free the attack up for another agent to pick up.
     event :abandon do
       transition running: :pending
       transition any => same
     end
 
+    ## Transitions
+
+    # Executed after an attack has entered the running state. This sets the start_time attribute to the current time.
     after_transition on: :running do |attack|
-      attack.update(start_time: Time.zone.now)
+      attack.touch(:start_time) # rubocop:disable Rails/SkipsModelValidations
+      attack.campaign.touch # rubocop:disable Rails/SkipsModelValidations
     end
 
+    # Executed after an attack has entered the completed state. This sets the end_time attribute to the current time.
     after_transition on: :complete do |attack|
-      attack.update(end_time: Time.zone.now)
+      attack.touch(:end_time) # rubocop:disable Rails/SkipsModelValidations
     end
 
+    # Executed after an attack has been abandoned. This removes all tasks associated with the attack.
     after_transition on: :abandon do |attack|
-      # If the attack is abandoned, we should remove the tasks to free up for another agent
+      # If the attack is abandoned, we should remove the tasks to free up for another agent and touch the campaign to update the updated_at timestamp
       attack.tasks.destroy_all
+      attack.campaign.touch # rubocop:disable Rails/SkipsModelValidations
     end
 
+    # Executed after an attack is being paused. This pauses all tasks associated with the attack.
     after_transition any => :paused, :do => :pause_tasks
+
+    # Executed after an attack is being resumed. This resumes all tasks associated with the attack.
     after_transition paused: any, do: :resume_tasks
 
+    # Executed after an attack has been completed. This completes the hash list for the campaign and updates the campaign's updated_at timestamp.
     after_transition any => :completed, :do => :complete_hash_list
+    after_transition any => :completed, :do => :touch_campaign
 
+    # Executed before an attack is marked completed. This completes all remaining tasks associated with the attack if the hash list is fully cracked.
     before_transition on: :complete do |attack|
       attack.tasks.each(&:complete!) if attack.hash_list.uncracked_count.zero?
     end
@@ -258,6 +261,10 @@ class Attack < ApplicationRecord
     state :pending
   end
 
+  ##
+  # Checks if the attack is completed.
+  #
+  # @return [Boolean] true if the attack is completed, otherwise false.
   def completed?
     self.state == "completed"
   end
@@ -356,6 +363,10 @@ class Attack < ApplicationRecord
     current_task&.progress_percentage || 0.00
   end
 
+  ##
+  # Generates a human-readable text for progress.
+  #
+  # @return [String, nil] the progress text.
   def progress_text
     current_task&.progress_text
   end
@@ -388,28 +399,26 @@ class Attack < ApplicationRecord
 
   private
 
+  # Generates the attack mode parameter for Hashcat.
+  #
+  # @return [String] the attack mode parameter.
   def attack_mode_param
     "-a #{Attack.attack_modes[attack_mode]}"
   end
 
   # Calculates the complexity for dictionary attack mode.
   #
-  # The complexity is calculated based on the number of lines in the word list and rule list.
-  # If the rule list is empty, the complexity is equal to the number of lines in the word list.
-  # Otherwise, the complexity is the product of the number of lines in the word list and the rule list.
+  # This method calculates the complexity based on the number of lines in the word list
+  # and the rule list. If the rule list is empty, the complexity is equal to the number
+  # of lines in the word list. If the rule list is not empty, the complexity is the product
+  # of the number of lines in the word list and the number of lines in the rule list.
   # If increment mode is enabled, the complexity is multiplied by the size of the increment range.
   #
-  # @return [BigDecimal] the calculated complexity for dictionary attack mode.
+  # @return [BigDecimal] the calculated complexity value.
   def calculate_dictionary_complexity
     word_list_lines = word_list&.line_count || 0
     rule_list_lines = rule_list&.line_count || 0
-
-    complexity = if rule_list_lines.zero?
-                   word_list_lines
-    else
-                   word_list_lines * rule_list_lines
-    end
-
+    complexity = rule_list_lines.zero? ? word_list_lines : word_list_lines * rule_list_lines
     complexity *= increment_range_size if increment_mode
     complexity.to_d
   end
@@ -421,6 +430,10 @@ class Attack < ApplicationRecord
     MaskCalculationMethods.calculate_mask_candidates(mask)
   end
 
+  # Generates the custom charset parameter for Hashcat.
+  #
+  # @param index [Integer] the index of the custom charset (1 to 4).
+  # @return [String, nil] the custom charset parameter if present, otherwise nil.
   def charset_param(index)
     value = send("custom_charset_#{index}")
     "-#{index} #{value}" if value.present?
@@ -457,14 +470,28 @@ class Attack < ApplicationRecord
     end
   end
 
+  # Returns the complexity value for a given element.
+  #
+  # @param element [String] the element for which to calculate the complexity value.
+  # @return [Integer] the complexity value for the given element.
   def complexity_value_for_element(element)
     COMPLEXITY_VALUES[element] || custom_charset_length(element) || 1
   end
 
+  # Returns the current running task for the attack.
+  #
+  # This method retrieves the task associated with the attack that is in the 'running' state,
+  # orders them by the 'updated_at' timestamp in descending order, and returns the first task in the list.
+  #
+  # @return [Task, nil] the current running task or nil if no such task exists.
   def current_task
     tasks.with_state(:running).order(updated_at: :desc).first
   end
 
+  # Returns the length of the custom charset for the given element.
+  #
+  # @param element [String] the element for which to retrieve the custom charset length.
+  # @return [Integer] the length of the custom charset.
   def custom_charset_length(element)
     case element
     when "?1" then custom_charset_1.length
@@ -476,15 +503,33 @@ class Attack < ApplicationRecord
     end
   end
 
+  # Generates the custom charset parameters for Hashcat.
+  #
+  # This method iterates through the custom charsets (1 to 4) and generates
+  # the corresponding parameters for each charset that is present.
+  #
+  # @return [String] A string of custom charset parameters.
   def custom_charset_params
     (1..4).map { |i| charset_param(i) }.compact.join(" ")
   end
 
+  # Generates the file parameters for Hashcat.
+  #
+  # This method retrieves the filenames of the word list and mask list,
+  # and includes the rule list file if present.
+  #
+  # @return [String] A string of file parameters.
   def file_params
     [word_list, mask_list].compact.map { |list| list.file.filename }.join(" ") +
       (rule_list.present? ? " -r #{rule_list.file.filename}" : "")
   end
 
+  # Generates the increment mode parameters for Hashcat.
+  #
+  # This method constructs the parameters for enabling increment mode in Hashcat,
+  # including the minimum and maximum increment values.
+  #
+  # @return [String] A string of increment mode parameters.
   def increment_mode_param
     ["--increment", "--increment-min #{increment_minimum}", "--increment-max #{increment_maximum}"].join(" ")
   end
@@ -496,12 +541,15 @@ class Attack < ApplicationRecord
     (increment_minimum..increment_maximum).to_a.size
   end
 
+  # Generates the Markov threshold parameter for Hashcat.
+  #
+  # @return [String] the Markov threshold parameter.
   def markov_threshold_param
     "--markov-threshold=#{markov_threshold}"
   end
 
-  # Pauses all tasks that are not currently in the paused state.
-  # Iterates through tasks that are not paused and calls the pause method on each.
+  # Pauses all tasks associated with the current object.
+  # Iterates through each task and calls the `pause` method on it.
   def pause_tasks
     tasks.without_state(:paused).each(&:pause)
   end
@@ -510,6 +558,16 @@ class Attack < ApplicationRecord
   # Iterates through each task and calls the `resume` method on it.
   def resume_tasks
     tasks.find_each(&:resume)
+  end
+
+  # Updates the `updated_at` timestamp of the associated campaign.
+  #
+  # This method calls the `touch` method on the campaign, which updates
+  # the `updated_at` timestamp to the current time.
+  #
+  # @return [void]
+  def touch_campaign
+    campaign.touch # rubocop:disable Rails/SkipsModelValidations
   end
 
   # Updates the stored complexity value of the attack.
